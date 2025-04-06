7d3f29d40a6c092aa2d647d4700cd37289280cd4
reachaki <akshyatdali@gmail.com>
2025-04-06 14:29:32 +0100

    panels

--- code/main.py
+++ code/main.py
@@ -1,6 +1,13 @@
 from settings import *
 from sys import exit
 
+# components
+from game import Game
+from score import Score
+from preview import Preview
+
+from random import choice
+
 
 class Main:
     def __init__(self):
@@ -11,6 +18,24 @@ class Main:
         self.clock = pygame.time.Clock()
         pygame.display.set_caption("Tetris")
 
+        # shapes
+        self.next_shapes = [choice(list(TETROMINOS.keys())) for shape in range(3)]
+
+        # components
+        self.game = Game(self.get_next_shape, self.update_score)
+        self.score = Score()
+        self.preview = Preview()
+
+    def update_score(self, lines, score, level):
+        self.score.lines = lines
+        self.score.score = score
+        self.score.level = level
+
+    def get_next_shape(self):
+        next_shape = self.next_shapes.pop(0)
+        self.next_shapes.append(choice(list(TETROMINOS.keys())))
+        return next_shape
+
     def run(self):
         while True:
             for event in pygame.event.get():
@@ -20,6 +45,11 @@ class Main:
 
             # display
             self.display_surface.fill(GRAY)
+
+            # components
+            self.game.run()
+            self.score.run()
+            self.preview.run(self.next_shapes)
 
             # updating the game
             pygame.display.update()
