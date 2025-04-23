b2bc4e21f15d8f2179c567cfac1cc79eba865faf
reachaki <akshyatdali@gmail.com>
2025-04-23 11:49:46 +0100

    input

--- code/game.py
+++ code/game.py
@@ -23,7 +23,10 @@ class Game:
         self.tetromino = Tetromino(choice(list(TETROMINOS.keys())), self.sprites)
 
         # timer
-        self.timers = {"vertical move": Timer(UPDATE_START_SPEED, True, self.move_down)}
+        self.timers = {
+            "vertical move": Timer(UPDATE_START_SPEED, True, self.move_down),
+            "horizontal move": Timer(MOVE_WAIT_TIME),
+        }
         self.timers["vertical move"].activate()
 
     def timer_update(self):
@@ -49,9 +52,21 @@ class Game:
 
         self.surface.blit(self.line_surface, (0, 0))
 
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if not self.timers["horizontal move"].active:
+            if keys[pygame.K_LEFT]:
+                self.tetromino.move_horizontal(-1)
+                self.timers["horizontal move"].activate()
+            if keys[pygame.K_RIGHT]:
+                self.tetromino.move_horizontal(1)
+                self.timers["horizontal move"].activate()
+
     def run(self):
 
         # update
+        self.input()
         self.timer_update()
         self.sprites.update()
 
@@ -73,6 +88,10 @@ class Tetromino:
 
         # create blocks
         self.blocks = [Block(group, pos, self.color) for pos in self.block_positions]
+
+    def move_horizontal(self, amount):
+        for block in self.blocks:
+            block.pos.x += amount
 
     def move_down(self):
         for block in self.blocks:
