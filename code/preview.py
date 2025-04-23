ee96feaabd91df4b867a4436fab4c37ecda57842
reachaki <akshyatdali@gmail.com>
2025-04-23 11:35:33 +0100

    shape, graphics & sound

--- code/preview.py
+++ code/preview.py
@@ -1,10 +1,13 @@
-from settings import * 
+from settings import *
+
 
 class Preview:
-	def __init__(self):
-		self.display_surface = pygame.display.get_surface()
-		self.surface = pygame.Surface((SIDEBAR_WIDTH, GAME_HEIGHT * PREVIEW_HEIGHT_FRACTION))
-		self.rect = self.surface.get_rect(topright = (WINDOW_WIDTH - PADDING,PADDING))
+    def __init__(self):
+        self.display_surface = pygame.display.get_surface()
+        self.surface = pygame.Surface(
+            (SIDEBAR_WIDTH, GAME_HEIGHT * PREVIEW_HEIGHT_FRACTION)
+        )
+        self.rect = self.surface.get_rect(topright=(WINDOW_WIDTH - PADDING, PADDING))
 
-	def run(self):
-		self.display_surface.blit(self.surface, self.rect)
+    def run(self):
+        self.display_surface.blit(self.surface, self.rect)
